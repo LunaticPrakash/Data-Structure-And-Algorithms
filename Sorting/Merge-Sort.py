a221b32a57a75694b15bb1b0ad105ad10adb12f9
Prakash Gupta <56812557+LunaticPrakash@users.noreply.github.com>
2020-09-12 12:31:49 +0530

    Update Merge-Sort.py

--- Sorting/Merge-Sort.py
+++ Sorting/Merge-Sort.py
@@ -42,8 +42,7 @@ def mergeSort(A):
             A[k] = C[j]
             k = k + 1
             j = j + 1
-    #returning final merged sorted array
-    return A
+
 
 #Taking the input
 array = list(map(int,input("Enter your array (as space separated integers) : ").split()))
