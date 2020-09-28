5c675180f249a3563c94099f5d23433e72b450d3
LunaticPrakash <prakash.25gupta@gmail.com>
2020-09-28 09:40:43 +0530

    Array-Left-Rotation-By-d commited

---
+++ Array/Array-Left-Rotation-By-d.py
@@ -0,0 +1,19 @@
+def LeftRotationOneTime(arr,d):
+
+    temp = arr[0]
+    for i in range(1,len(arr)):
+        arr[i-1] = arr[i]
+
+    arr[i] = temp
+
+
+def leftRotateby(arr, d): 
+    for i in range(d): 
+        LeftRotationOneTime(arr, len(arr)) 
+
+
+array = list(map(int,input("Enter your array (as space separated integers) : ").split()))
+d = int(input("How many times you want to rotate? "))
+
+leftRotate(array,d)
+print(array)
