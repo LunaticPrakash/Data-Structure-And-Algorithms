e749c37a4ef5d7955309cd8818c49833f0b2c1f3
LunaticPrakash <prakash.25gupta@gmail.com>
2021-09-14 11:03:36 +0530

    Updated Tree.java

--- Java/BinaryTree/Tree.java
+++ Java/BinaryTree/Tree.java
@@ -1,5 +1,3 @@
-package BinaryTree;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -256,8 +254,8 @@ class BinaryTreeDS {
     // top view of tree
     public ArrayList<Integer> topView(Node root) {
         ArrayList<Integer> ans = new ArrayList<>();
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        Queue<Pair<Node, Integer>> queue = new LinkedList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>(); // hd, node
+        Queue<Pair<Node, Integer>> queue = new LinkedList<>(); // (node, hd)
 
         if (root == null)
             return ans;
@@ -414,13 +412,13 @@ class BinaryTreeDS {
     }
 
     // check if tree is balanced
-    public boolean isBalanced(Node root){
-        if(root == null)
+    public boolean isBalanced(Node root) {
+        if (root == null)
             return true;
         int lh = height(root.left);
         int rh = height(root.right);
 
-        if(Math.abs(lh-rh) <= 1 && isBalanced(root.left) && isBalanced(root.right))
+        if (Math.abs(lh - rh) <= 1 && isBalanced(root.left) && isBalanced(root.right))
             return true;
         return false;
     }
