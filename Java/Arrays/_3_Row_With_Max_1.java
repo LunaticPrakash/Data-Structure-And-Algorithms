a84003bfc71556b501378ac2987ca21f4b5b3b13
LunaticPrakash <prakash.25gupta@gmail.com>
2021-09-14 05:43:36 +0530

    .

--- Java/Arrays/_3_Row_With_Max_1.java
+++ Java/Arrays/_3_Row_With_Max_1.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
+
 public class _3_Row_With_Max_1 {
 
-    private static int getRow(int arr[][], int n, int m) {
-        int maxCount1 = 0;
-        int maxCountRow = -1;
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 1) {
-                    count += 1;
-                    if (count > maxCount1) {
-                        maxCount1 = count;
-                        maxCountRow = i;
-                    }
-                }
-            }
-        }
-        return maxCountRow;
-    }
+    // private static int getRow(int arr[][], int n, int m) {
+    //     int maxCount1 = 0;
+    //     int maxCountRow = -1;
+    //     for (int i = 0; i < n; i++) {
+    //         int count = 0;
+    //         for (int j = 0; j < m; j++) {
+    //             if (arr[i][j] == 1) {
+    //                 count += 1;
+    //                 if (count > maxCount1) {
+    //                     maxCount1 = count;
+    //                     maxCountRow = i;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return maxCountRow;
+    // }
 
     private static int getSum(int[] arr) {
         int sum = 0;
@@ -54,7 +55,7 @@ public class _3_Row_With_Max_1 {
             for (int j = 0; j < c; j++)
                 arr[i][j] = Integer.parseInt(line[j]);
         }
-
         System.out.println("Row with max 1 = " + rowWithMax1s(arr, r, c));
+        scan.close();
     }
 }
