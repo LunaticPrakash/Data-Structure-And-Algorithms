cbb5cd51e33e4428b5dc6e8f574d091445b08b4a
Prakash Gupta <prakash.25gupta@gmail.com>
2023-01-31 14:29:12 +0530

    Added more function.

--- Java/Tree/BinarySearchTree.java
+++ Java/Tree/BinarySearchTree.java
@@ -64,6 +64,49 @@ public class BinarySearchTree {
         return ans;
     }
 
+    // Search a TreeNode: Recursive - Time: O(H) Space: O(H)
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val)
+            return root;
+        if (root.val < val)
+            return searchBST(root.right, val);
+        return searchBST(root.left, val);
+    }
+
+    // Sorted Array to BST: Recursive - Time: O(N) Space: O(N)
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBSTHelper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBSTHelper(int nums[], int l, int r) {
+        if (l > r)
+            return null;
+        int mid = (l + r) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBSTHelper(nums, l, mid - 1);
+        root.right = sortedArrayToBSTHelper(nums, mid + 1, r);
+        return root;
+    }
+
+    // BST from Preorder: Recursive - Time: O(N) Space: O(N)
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return bstFromPreorderHelper(preorder, 0, preorder.length - 1);
+    }
+
+    private TreeNode bstFromPreorderHelper(int[] preorder, int start, int end) {
+        if (start > end)
+            return null;
+        int rootVal = preorder[start];
+        TreeNode root = new TreeNode(rootVal);
+        int i = start + 1;
+        while (i <= end && preorder[i] < rootVal) {
+            i++;
+        }
+        root.left = bstFromPreorderHelper(preorder, start+1, i-1);
+        root.right = bstFromPreorderHelper(preorder, i, end);
+        return root;
+    }
+
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
         TreeNode root = new TreeNode(1);
